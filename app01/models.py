032be666b2c12b26d885fb03195010cfca3f2a0f
liangyufeng <liagnyufeng@qq.com>
2019-04-29 16:15:24 +0800

    创建model类

--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=32)  # 书名
+    price = models.CharField(max_length=32)  # 价格
+    author = models.CharField(max_length=32)  # 作者
+    publisher = models.CharField(max_length=32)  # 出版社
